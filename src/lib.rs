90be1bd66156c6d0261e154bd349a34d44763a58
Krishna Rathnam <kr130205@gmail.com>
2026-01-29 19:28:43 +0530

    project finihed

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
 }
